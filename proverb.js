e2ee10e113639e355f804a5eae4ac510d88c1531
Tim Tolley <Tim@Tims-MBP.fritz.box>
2017-11-05 15:03:26 +1300

    continued Api attempt 5

--- proverb.js
+++ proverb.js
@@ -1,7 +1,18 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://eda-te-reo.herokuapp.com/api/proverbs", false);
-xhr.send();
 
-console.log(xhr.status);
-console.log(xhr.statusText);
-console.log(xhr.response);
+var proverb = document.getElementById("proverb");
+var genButton = document.getElementById("proverb-generator");
+
+
+
+function getProverb(){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://eda-te-reo.herokuapp.com/api/proverbs", false)
+  xhr.onload =function (){
+    var newProverb= JSON.parse(xhr.responseText);
+    console.log(newProverb[0]);
+  }
+  xhr.send();
+};
+
+
+genButton.addEventListener("click", getProverb)
