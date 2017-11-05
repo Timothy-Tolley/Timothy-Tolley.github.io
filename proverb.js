60a06d5bae63264671ef646388a946641294d7d2
Tim Tolley <Tim@Tims-MBP.fritz.box>
2017-11-05 14:17:29 +1300

    new attempt to make API

--- proverb.js
+++ proverb.js
@@ -1,19 +1,7 @@
-var proverbDiv =document.getElementById('proverb');
-var proverbGenerator = document.getElementById('proverb-generator');
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://eda-te-reo.herokuapp.com/api/proverbs", false);
+xhr.send();
 
-proverbGenerator.addEventListener('click', function(){
-var getProverb = new XMLHttpRequest();
-getProverb.open('GET', 'https://eda-te-reo.herokuapp.com/api/proverbs')
-getProverb.onload= function(){
-  var myProverb =JSON.parse(getProverb.responseText);
-  var myProverb= getProverb.responseText;
-  addProverb(myProverb)
-}
-
-getProverb.send();
-});
-
-function addProverb(proverb){
-
-
-}
+console.log(xhr.status);
+console.log(xhr.statusText);
+console.log(xhr.response);
